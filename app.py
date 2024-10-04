724f47c141e545b195627aeeee912eba1399b6ae
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-10-04 20:40:19 +0100

    For tiktok

--- app.py
+++ app.py
@@ -12,3 +12,16 @@ def download_youtube_video(url, save_path):
     except Exception as e:
         print(f"Error downloading YouTube video: {str(e)}")
 
+def download_tiktok_video(url, save_path):
+    try:
+        api = TikTokApi()
+        video = api.video(url=url)
+        video_data = video.bytes()
+        video_id = video.id
+        file_path = os.path.join(save_path, f'tiktok_{video_id}.mp4')
+        with open(file_path, 'wb') as f:
+            f.write(video_data)
+        print(f'TikTok video downloaded: {file_path}')
+    except Exception as e:
+        print(f"Error downloading TikTok video: {str(e)}")
+
