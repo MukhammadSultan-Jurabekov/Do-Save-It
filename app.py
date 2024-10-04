a2405803ca3eda6f66104f232cb82c625093f947
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-10-04 20:40:11 +0100

    For Youtube

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from pytube import YouTube
 from TikTokApi import TikTokApi
 import instaloader
 
+def download_youtube_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download(save_path)
+        print(f'YouTube video downloaded: {stream.title}')
+    except Exception as e:
+        print(f"Error downloading YouTube video: {str(e)}")
+
